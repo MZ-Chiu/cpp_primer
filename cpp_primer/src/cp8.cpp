73cccb873f0e79a4435828e46c7e1abf18848ba3
qiumz <qmz1592@163.com>
2020-05-02 01:40:23 +0800

    [ADD] Add chapter 8 exercise

--- cpp_primer/src/cp8.cpp
+++ cpp_primer/src/cp8.cpp
@@ -1,7 +1,8 @@
 #include "cp8.hpp"
+#include <fstream>
 
 std::istream &read(std::istream &s) {
-	char c;
+	string c;
 
 	while (s >> c) {
 		cout << c;
@@ -21,17 +22,28 @@ void ex8_1(void) {
 
 }
 
+void ex8_2(void) {
+	std::fstream f1("F:/workspace/CPP/cpp_primer/data/string.txt", std::ios::in | std::ios::in);
+	std::vector<string> s_vec;
+	string s;
+
+	if (!f1.is_open()) {
+		cout << "File open fail" << endl;
+	}
+	else {
+		//while (f1 >> s) { // get word
+		while (getline(f1, s)) { // get linn
+			cout << s << endl;
+			//s_vec.push_back(s);
+		}
+	}
+}
+
 void cp8_loop(void) {
 	cout << "Welcom to cp8" << endl;
 
-	char c1, c2, c3;
-
-	cin >> c1;
-	cout << c1 << endl;
-	cin >> c2;
-	cout << c2 << endl;
-	cin >> c3;
-	cout << c3 << endl;
 	//ex8_1();
+	
+	ex8_2();
 
 }
