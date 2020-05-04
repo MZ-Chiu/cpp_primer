d94f362a1c1f9241bc11dc1d59cf3a17a42d6285
qiumz <qmz1592@163.com>
2020-05-04 23:32:55 +0800

    [ADD] Add exercise 8.3

--- cpp_primer/src/cp8.cpp
+++ cpp_primer/src/cp8.cpp
@@ -1,12 +1,13 @@
 #include "cp8.hpp"
 #include "cp7.hpp"
 #include <fstream>
+#include <sstream>
 
 std::istream &read(std::istream &s) {
 	string c;
 
 	while (s >> c) {
-		cout << c;
+		cout << c << " ";
 	}
 	cout << endl;
 	s.clear();
@@ -50,11 +51,117 @@ void ex8_2(void) {
 	}
 }
 
+bool isValidPhoneNum(const string &num) {
+	return num.size() == 11;
+}
+
+string formatPhoneNum(const string &num) {
+	string s = num;
+	s.insert(0, "+86");
+	return s;
+}
+
+void ex8_3(void) {
+	// 8.9
+	//string s;
+
+	//cout << "Enter a string" << endl;
+
+	//getline(cin, s);
+	//std::stringstream ss(s);
+
+	//read(ss);
+
+	// 8.10
+	//std::ifstream f_in("F:/workspace/CPP/cpp_primer/data/salesData.txt");
+
+	//string s;
+	//std::vector<string> svec;
+	//while (getline(f_in, s)) {
+	//	svec.push_back(s);
+	//}
+	//for (auto s : svec) {
+	//	std::stringstream ss(s);
+	//	read(ss);
+	//}
+
+	// 8.11
+	//string line;
+	//std::stringstream record;
+	//std::vector<personInfo> phoneBook;
+
+	//cout << "Enter some personInfo(name phone1 phone2...)" << endl;
+	//while (getline(cin, line)) {
+	//	string phone;
+	//	personInfo info;
+
+	//	record.str(line);
+	//	record >> info.name;
+	//	while (record >> phone) {
+	//		info.phones.push_back(phone);
+	//	}
+	//	record.clear();
+	//	phoneBook.push_back(info);
+	//}
+
+	//cout << "name\t" << "phone" << endl;
+	//for (auto info : phoneBook) {
+	//	cout << info.name << "\t";
+	//	for (auto phone : info.phones) {
+	//		cout << phone << "\t\t";
+	//	}
+	//	cout << endl;
+	//}
+
+	// 8.13
+	string line;
+	std::stringstream record;
+	std::vector<personInfo> phoneBook;
+	std::ifstream f_in("F:/workspace/CPP/cpp_primer/data/phoneBook.txt");
+	std::ofstream f_out("F:/workspace/CPP/cpp_primer/data/phoneBook1.txt", std::ios::app);
+
+	cout << "Reading phoneBook..." << endl;
+	while (getline(f_in, line)) {
+		string phone;
+		personInfo info;
+
+		record.str(line);
+		record >> info.name;
+		while (record >> phone) {
+			info.phones.push_back(phone);
+		}
+		record.clear();
+		phoneBook.push_back(info);
+	}
+
+	cout << "name\t\tphone(Correct phone num will be write to new file)" << endl;
+	for (auto info : phoneBook) {
+		std::ostringstream formatted, badNums;
+
+		for (const auto phone : info.phones) {
+			if (isValidPhoneNum(phone)) {
+				formatted << formatPhoneNum(phone) << " ";
+			}
+			else {
+				badNums << phone << " ";
+			}
+		}
+		if (badNums.str().empty()) {
+			cout << info.name << "\t\t" << formatted.str() << endl;
+			f_out << info.name << "\t\t" << formatted.str() << endl;
+		}
+		else {
+			std::cerr << info.name << "\t\t#invalid phone num: " << badNums.str() << endl;
+		}
+	}
+}
+
 void cp8_loop(void) {
 	cout << "Welcom to cp8" << endl;
 
 	//ex8_1();
 	
-	ex8_2();
-
+	//ex8_2();
+	
+	ex8_3();
 }
--- cpp_primer/src/cp8.hpp
+++ cpp_primer/src/cp8.hpp
@@ -2,5 +2,11 @@
 #include <vector>
 #include "common.hpp"
 
+class personInfo {
+public:
+	string name;
+	std::vector<string> phones;
+};
+
 
 void cp8_loop(void);
