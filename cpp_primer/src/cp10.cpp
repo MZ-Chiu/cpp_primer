46e6a29cd618113ae9c848517b9221150df0c049
qiumz <qmz1592@163.com>
2020-05-18 01:16:37 +0800

    [ADD] Add exercise to 10.21

--- cpp_primer/src/cp10.cpp
+++ cpp_primer/src/cp10.cpp
@@ -17,15 +17,124 @@ void ex10_1() {
 }
 
 void ex10_2() {
-	std::vector<int> iv = {1, 2, 3, 4,5, 6, 7, 8, 9, 10};
-
-	int sum = std::accumulate(iv.cbegin(), iv.cend(), 0);
-	cout << "Sum of iv:" << sum << endl;
-
+	//// 10.3
+	//std::vector<int> iv = {1, 2, 3, 4,5, 6, 7, 8, 9, 10};
+	//int sum = std::accumulate(iv.cbegin(), iv.cend(), 0);
+	//cout << "Sum of iv:" << sum << endl;
+	//// 10.4
 	//std::vector<double> vd = { 1.1, 0.5, 3.3 };
-	//std::cout << "ex 10.04: "
-	//	<< std::accumulate(vd.cbegin(), vd.cend(), 0)
-	//	<< std::endl;
+	//cout << "ex 10.04: " << std::accumulate(vd.cbegin(), vd.cend(), 0.0) << std::endl;
+
+	// 10.5
+	// 10.6
+	//std::vector<int> iv = { 1, 2, 3, 4, 5, 6 };
+	//std::fill_n(iv.begin(), iv.size(), 0);
+	//for (auto i : iv) {
+	//	cout << i << " ";
+	//}
+	//cout << endl;
+	// 10.7
+	// a. vec is empty
+	// b. vec is empty
+	// 10.8
+	// casue the algotithm call back_insert, who will increase the container size.
+
+	// 10.9
+	std::vector<string> svec = { "the", "quick", "red", "fox", 
+		"jumps", "over", "the", "slow", "red", "turtle"};
+	std::sort(svec.begin(), svec.end());
+	auto end_unique = std::unique(svec.begin(), svec.end());
+	svec.erase(end_unique, svec.end());
+
+	for (auto s : svec) {
+		cout << s << " ";
+	}
+	cout << endl;
+
+	// 10.10
+	// 
+}
+
+bool isShorter(const string &a, const string &b) {
+	return a.size() < b.size();
+}
+
+void elimDups(std::vector<string> &words) {
+	std::sort(words.begin(), words.end());
+	auto end_unique = std::unique(words.begin(), words.end());
+	words.erase(end_unique, words.end());
+}
+
+void lambda_foo() {
+	int a = 2;
+	auto sum = [a](int b) {return a + b; };
+	cout << sum(3) << endl;
+}
+
+void biggies(std::vector<string> &words, std::vector<string>::size_type sz) {
+	elimDups(words);
+	std::stable_sort(words.begin(), words.end(), isShorter);
+	auto wc = std::find_if(words.begin(), words.end(), [sz](const string &s) { return s.size() > sz; });
+	std::for_each(wc, words.end(), [](const string &s) { cout << s << " "; });
+	cout << endl;
+}
+
+void biggies1(std::vector<string> &words, std::vector<string>::size_type sz) {
+	elimDups(words);
+	std::stable_sort(words.begin(), words.end(), isShorter);
+	auto end = std::stable_partition(words.begin(), words.end(), [sz](const string &s) { return s.size() > sz; });
+	auto wc = std::count_if(words.begin(), words.end(), [sz](const string &s) { return s.size() > sz; });
+	cout << "There are " << wc << " words's size longger then " << sz << endl;
+	std::for_each(words.begin(), end, [](const string &s) { cout << s << " "; });
+	cout << endl;
+}
+
+void ex10_3(void) {
+	// 10.11
+	//std::vector<string> words = { "the", "quick", "red", "fox",
+	//	"jumps", "over", "the", "slow", "red", "turtle" };
+	//elimDups(words);
+	//std::stable_sort(words.begin(),words.end(), isShorter);
+	//std::for_each(words.begin(), words.end(), [](const string &s) { cout << s << " "; });
+
+	// 10.12
+	// 10.13
+	//auto end = std::partition(words.begin(), words.end(), [](const string &s) {return s.size() > 4; });
+	//std::for_each(words.begin(), end, [](const string &s) { cout << s << " "; });
+
+	// 10.14
+	//auto sum = [] (int a, int b) { return a + b; };
+	//cout << sum(1, 2) << endl;
+
+	// 10.15
+	//lambda_foo();
+
+	// 10.16
+	//std::vector<string> words = { "the", "quick", "red", "fox",
+	//	"jumps", "over", "the", "slow", "red", "turtle" };
+	//biggies(words, 4);
+	// 10.18, 10.19, 10.20
+	//biggies1(words, 4);
+
+	// 10.21
+	int a = 10;
+	auto f = [](int &a) -> bool { 
+		if (a > 0) {
+			--a; 
+			return false;
+		}
+		else {
+			return true;
+		}
+	};
+
+	cout << "a = " << a << endl;
+	while (1) {
+		if (f(a)) {
+			cout << "a = " << a << endl;
+			break;
+		}
+	}
 }
 
 void cp10_loop(void) {
@@ -33,5 +142,7 @@ void cp10_loop(void) {
 
 	//ex10_1();
 
-	ex10_2();
+	//ex10_2();
+
+	ex10_3();
 }
