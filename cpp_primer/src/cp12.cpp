14f044b89e0fc8c0b4b4b6d1b61c2960987807d6
qiumz <qmz1592@163.com>
2020-06-01 01:14:24 +0800

    [ADD] 12.1 - 12.9 ex

--- cpp_primer/src/cp12.cpp
+++ cpp_primer/src/cp12.cpp
@@ -24,17 +24,68 @@ private:
 	std::shared_ptr<std::vector<string>> data;
 };
 
+std::vector<int> *new_vector() {
+	return new std::vector<int>;
+}
+
+std::shared_ptr<std::vector<int>> make_shared_vector() {
+	return std::make_shared<std::vector<int>>();
+}
+
+void fill_vector(std::vector<int> *p) {
+	int n = 0;
+	cout << "This is new, enter some numbers:" << endl;
+	cin.clear();
+	while (cin >> n) {
+		p->push_back(n);
+	}
+
+	for (auto i : *p) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+void fill_vector(std::shared_ptr<std::vector<int>> p) {
+	int n = 0;
+	cout << "This is share pointer, enter some numbers:" << endl;
+	cin.clear();
+	while (cin >> n) {
+		p->push_back(n);
+	}
+
+	for (auto i : *p) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
 
 void ex12_1(void) {
 	// 12.1 - 12.5
-	strBlob sb({ "a", "an" });
-	sb.push_back("im qmz");
-	sb.print();
-	cout << sb.front() << " " << endl;
-	cout << sb.back() << " " << endl;
+	//strBlob sb({ "a", "an" });
+	//sb.push_back("im qmz");
+	//sb.print();
+	//cout << sb.front() << " " << endl;
+	//cout << sb.back() << " " << endl;
 
-	const strBlob csb({ "aa", "aan" });
-	//csb.push_back("xyz");
+	//const strBlob csb({ "aa", "aan" });
+	////csb.push_back("xyz");
+
+	// 12.6, 12.7
+	//std::vector<int> *p_vec = new_vector();
+	//std::shared_ptr<std::vector<int>> p_share_vec = make_shared_vector();
+
+	//fill_vector(p_vec);
+	//fill_vector(p_share_vec);
+	//delete p_vec;
+	//p_vec = NULL;
+
+	// 12.8
+	/* return pointer for bool, error */
+
+	// 12.9
+	/* old r will cause memoty leakage */
+	/* old r2 will be released automatically */
 
 }
 
